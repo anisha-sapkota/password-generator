5e584b6e699d96cc4c3671b3431e7e5009ac7041
Anisha Sapkota <anisha.sapkota79@gmail.com>
2022-08-16 16:26:48 +1000

    added generatePassword function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,20 @@
-// Assignment code here
+// function for generating password``
+function generatePassword() {
+  var passwordLength = window.prompt(
+    "Please enter password length (between 8 and 128 character)"
+  );
+  var incLowercase = window.confirm("Include lowercase characters?");
+  var incUppercase = window.confirm("Include uppercase characters?");
+  var incNumbers = window.confirm("Include numbers?");
+  var incSpecialChars = window.confirm("Include special characters?");
 
+  if (incLowercase || incUppercase || incNumbers || incSpecialChars) {
+    return "password";
+  } else {
+    window.alert("At least one character type should be selected!");
+    generatePassword();
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +25,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
